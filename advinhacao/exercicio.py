dca024d7799d3ecfb97bd6ed75e45245d8dd4a61
brenoyure <byurifb@gmail.com>
2022-08-07 02:27:56 -0300

    .

--- advinhacao/exercicio.py
+++ advinhacao/exercicio.py
@@ -3,21 +3,32 @@ print("# Bem-Vindo ao Jogo da Advinhação #")
 print("###################################")
 
 numero_secreto = 42
+total_de_tentativas = 3
 
-chute_str = input("Digite um Número: ")
-chute = int(chute_str)
+for rodada in range(1, total_de_tentativas + 1):  # Usando o laço for para incrementar as rodadas
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite um Número entre 1 e 100: ")
+    chute = int(chute_str)
 
-print("Você digitou", chute)
+    print("Você digitou", chute)
 
-acertou = chute == numero_secreto
-maior   = chute > numero_secreto
-menor   = chute < numero_secreto
+    if chute < 1 or chute > 100:
+        print("Você deve digitar um número entre 1 e 100.")
+        continue  # Usamos o continue para pular para a próxima iteração.
 
-if acertou:
-    print("Parabéns, você acertou")
-elif maior:
-    print("Chute foi Maior")
-else:
-    print("Chute Foi Menor")
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-print("Fim do Jogo")
+    if acertou:
+        print("Parabéns, você acertou.")
+        break  # Usamos o break para sair do laço for quando o usuário acertar.
+    else:
+        if maior:
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        elif menor:
+            print("Você errou! O seu chute foi menor que o número secreto.")
+
+#    rodada = rodada + 1  <-- não precisa mais somar, pois o laço for já faz isso.
+
+print("Fim de Jogo")
--- advinhacao/main.py
+++ advinhacao/main.py
@@ -1,28 +1,32 @@
 print("###################################")
-print("# Bem vindo ao Jogo de Advinhação #")
+print("# Bem-Vindo ao Jogo da Advinhação #")
 print("###################################")
 
 numero_secreto = 42
 total_de_tentativas = 3
-rodada = 1
 
-while rodada <= total_de_tentativas:
-    print("Tantativa {} de {}".format(rodada, total_de_tentativas))
-    chute_str = input("Digite um número: ")
-    print("Você digitou ", chute_str)
+for rodada in range(1, total_de_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite um Número entre 1 e 100: ")
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100.")
+        continue
+
+    print("Você digitou", chute)
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
     menor = chute < numero_secreto
 
     if acertou:
-        print("Você Acertou")
-        total_de_tentativas = 0
+        print("Parabéns, você acertou.")
+        break
     else:
         if maior:
-            print("Chute foi maior")
-            rodada = rodada + 1
+            print("Você errou! O seu chute foi maior que o número secreto.")
         elif menor:
-            print("Chute foi Menor")
-            rodada = rodada + 1
+            print("Você errou! O seu chute foi menor que o número secreto.")
+
+print("Fim do Jogo")
